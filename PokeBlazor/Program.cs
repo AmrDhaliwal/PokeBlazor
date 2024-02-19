2fd1f4a4f0c2cf28f34f09f0afc4ff289dd8adc5
Amrit Dhaliwal <amritdhaliwalz@gmail.com>
2024-02-19 05:45:40 +0000

    Base project is born using dotnet template for Blazor.

---
+++ PokeBlazor/Program.cs
@@ -0,0 +1,31 @@
+using PokeBlazor.Client.Pages;
+using PokeBlazor.Components;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorComponents()
+    .AddInteractiveServerComponents()
+    .AddInteractiveWebAssemblyComponents();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseWebAssemblyDebugging();
+}
+else
+{
+    app.UseExceptionHandler("/Error", createScopeForErrors: true);
+}
+
+app.UseStaticFiles();
+app.UseAntiforgery();
+
+app.MapRazorComponents<App>()
+    .AddInteractiveServerRenderMode()
+    .AddInteractiveWebAssemblyRenderMode()
+    .AddAdditionalAssemblies(typeof(PokeBlazor.Client._Imports).Assembly);
+
+app.Run();
